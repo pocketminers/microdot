f602ca4ae0dd3f82fde77df2118efaee3bf47f19
boyroywax <jmsdevln@gmail.com>
2025-01-10 12:06:32 -0800

    Add initial implementation of artifacts and utilities; include Argument and checks classes

---
+++ dist/utils/checks.js
@@ -0,0 +1,35 @@
+const checkForCircularReference = (obj) => {
+    const seen = new WeakSet();
+    const hasCircular = (obj) => {
+        if (obj !== null && typeof obj === "object") {
+            if (seen.has(obj)) {
+                return true;
+            }
+            seen.add(obj);
+            for (const key in obj) {
+                if (hasCircular(obj[key])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+    return hasCircular(obj);
+};
+const checkIsEmpty = (values) => {
+    if (values !== undefined &&
+        values !== null) {
+        for (const value of values) {
+            if (value === undefined ||
+                value === null ||
+                value === "") {
+                return true;
+            }
+        }
+    }
+    else {
+        return true;
+    }
+    return false;
+};
+export { checkForCircularReference, checkIsEmpty };
